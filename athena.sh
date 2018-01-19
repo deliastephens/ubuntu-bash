f23ee3358b75505aa7bebe3f6afbfd2999612423
Delia <deliasstephens@gmail.com>
2018-01-19 18:51:27 -0500

    removed directory code

--- athena.sh
+++ athena.sh
@@ -12,20 +12,9 @@ git config --global user.email
 echo "Making git remember ye olde password"
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
-echo "Making code directory"
-cd ~/Documents/
-mkdir code
-cd code/
-echo "Cloning current repositories." 
-echo "interrobang"
-git clone https://github.com/deliastephens/interrobang.git
-echo "flight side"
-git clone https://github.com/deliastephens/flight-side.git
 
 # Python / 6s080 Configuration
 echo "Installing python..."
 sudo apt-get install python3
 sudo apt-get install python3-tk
 sudo apt-get install idle3
-mkdir 6s080
-cd 6s080
